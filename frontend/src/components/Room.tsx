0409eb4fc6aba4fa5b85fa9fe2e5d3490576eb4f
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-03 18:42:09 +0530

    render-og

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Socket, io } from "socket.io-client";
 
-const URL = "https://omegle-backend-git-master-nevil844.vercel.app";
+const URL = "https://omegle-po2g.onrender.com";
 // const URL = "http://localhost:3000";
 
 export const Room = ({

d76af583bb03b5d26015b9e2bb639f236ea6975a
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-03 18:47:33 +0530

    render-gawd

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Socket, io } from "socket.io-client";
 
-const URL = "https://master--omegl.netlify.app";
+const URL = "https://omegle-po2g.onrender.com";
 // const URL = "http://localhost:3000";
 
 export const Room = ({

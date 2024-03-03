8a5fafd3a40bd33b18504fb7990bf41dc8554482
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-03 16:08:33 +0530

    deploy

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Socket, io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = "http://localhost:3000"|| "https://omegle-1.onrender.com/";
 
 export const Room = ({
   name,

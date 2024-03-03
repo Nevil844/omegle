4ae2fe4f4b7def07ae16558c8b2e8f0df4a0600b
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-03 17:02:04 +0530

    back

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import { Server } from 'socket.io';
 import { UserManager } from "./managers/UserManger";
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(http);
 
 const io = new Server(server, {
   cors: {
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -30,7 +30,7 @@ export const Room = ({
   const localVideoRef = useRef<HTMLVideoElement>();
 
   useEffect(() => {
-    var socket = io();
+    var socket = io(URL);
     socket.on("send-offer", async ({ roomId }) => {
       console.log("sending offer");
       setLobby(false);

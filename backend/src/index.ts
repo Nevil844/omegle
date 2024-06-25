dbf274072dd78147ff6e647a917b480c1f713f9b
Nevil844 <nevil.j@ahduni.edu.in>
2024-06-25 19:05:58 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,47 @@
 import { Socket } from "socket.io";
 import { createServer } from "http";
-
 import express from 'express';
 import { Server } from 'socket.io';
 import { UserManager } from "./managers/UserManger";
 
+// Create an instance of Express
 const app = express();
 const server = createServer(app);
 const cors = require('cors');
 
-app.use(cors({
-  origin: '*', // Allow requests from this origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // If you need to send cookies or authentication
-}));
+// Configure CORS middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
+// Initialize Socket.io server with CORS configuration
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ['GET', 'POST'],
-    credentials: true
+    origin: "*", // Allow connections from any origin
+    methods: ['GET', 'POST'], // Allow these HTTP methods
+    credentials: true // Allow sending credentials
   }
 });
 
+// Create an instance of UserManager
 const userManager = new UserManager();
 
+// Set up Socket.io connection event
 io.on('connection', (socket: Socket) => {
   console.log('a user connected');
   userManager.addUser("randomName", socket);
+
+  // Set up disconnection event
   socket.on("disconnect", () => {
     console.log("user disconnected");
     userManager.removeUser(socket.id);
-  })
+  });
 });
 
-server.listen(3000,'0.0.0.0', () => {
-    console.log('listening on *:3000');
+// Start the server and listen on port 3000
+server.listen(3000, '0.0.0.0', () => {
+  console.log('listening on *:3000');
 });
